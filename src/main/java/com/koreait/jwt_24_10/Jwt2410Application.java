0ea0ca0252caf3eb3e009ab50b1c76c6787cc8bd
yeosang0508 <yeosang0508@gmail.com>
2024-10-28 13:07:26 +0900

    프로그램 세팅

---
+++ src/main/java/com/koreait/jwt_24_10/Jwt2410Application.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_24_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt2410Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt2410Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/jwt_24_10/Jwt2410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_24_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt2410ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
